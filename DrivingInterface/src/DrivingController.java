37abe99e5fd590925d0251dbd43490b160bfd6bc
MS <MS@DESKTOP-H42SSOL>
2016-10-16 02:38:32 +0900

    장애물 회피 로직 추가 -류민석

--- DrivingInterface/src/DrivingController.java
+++ DrivingInterface/src/DrivingController.java
@@ -37,16 +37,84 @@ public class DrivingController {
 		// To-Do : Make your driving algorithm
 		
 
+		double plusDegree=0;
+		double plusAcc=0;
+		int frontCar = getIndexNearFrontCar(toMiddle,dist_cars); //앞차의 index를 받아옴
+		
+		if(frontCar>=0){
+			plusDegree = getDegreeNearFrontCar(toMiddle,frontCar,frontCar+1);
+			if(plusDegree * 180/Math.PI >= 60) {
+				System.out.println("PlusDegree over 60"+" " + plusDegree * 180/Math.PI);
+				plusDegree = 0;
+			}
+		}
+		//index가 0이상이면 50m이내의 앞차가 있다는 의미 60도 이상일땐 핸들을 꺾지않음
+		System.out.println("PlusDegree "+plusDegree * 180/Math.PI);
+		
+		if(plusDegree >0) { 
+			angle -= (plusDegree);
+		}
+		//왼쪽으로 피할것인가 오른쪽으로 피할것인가
+		else if(plusDegree <0) {
+			angle +=  (plusDegree);
+		} else {
+			// 60도이상 벌어져있으면 피할필요가 없다고 판단, 가속도 증가
+			if(toDegree(angle)<=60) plusAcc += 0.1;
+		}
+		
+		
+		
+		
+		
 		////////////////////// output values		
-		cmd.steer = angle*4+(-1*toMiddle/100);
-		cmd.accel = 0.2;
+		cmd.steer = 3.5*angle+(-1*toMiddle/100);
+		cmd.accel = 0.2+plusAcc;
 		cmd.brake = 0.0;
 		cmd.backward = DrivingInterface.gear_type_forward;
 		////////////////////// END output values
 		
+		
 		return cmd;
 	}
 	
+	//제일 가까이 붙어있는 차의 index
+	
+	public static int getIndexNearFrontCar(double myMiddle,double cars[]){
+		int c=-1;
+		
+		double minDist = 50; 
+		double minMiddle = 1;
+		//범위 지정을 위함 검색은 1m ~ 50m
+		
+		for(int i =0;i<cars.length;i+=2){
+			if(cars[i]>=0 && Math.abs(cars[i+1]-myMiddle)>=2){
+				//>=0 은 사실 별의미없음 거리가 0m이상 떨어져있냐는것
+				double dist = Math.sqrt(cars[i]*cars[i]+Math.abs(cars[i+1]-myMiddle)*Math.abs(cars[i+1]-myMiddle));
+				if(minDist >= dist) {
+					minDist = dist;
+					c = i;
+				}
+				
+			}
+		}
+		if(c>=0){	System.out.println("Near Dist : " + minDist);}
+		return c;
+	}
+	
+	//제일 가까이 붙어있는 차와의 각도 
+	public static double getDegreeNearFrontCar(double myMiddle,double car,double carMiddle){
+		double c=0;
+				
+		c = Math.atan2(Math.abs(myMiddle-carMiddle), car) - Math.PI/2 ;
+		
+		
+		System.out.println("Near Degree : " + c + " "+c*180/Math.PI);
+		return c;
+	}
+	//디버깅을 위한 각도 변환
+	public static double toDegree(double rad){
+		return rad * 180/Math.PI;
+	}
 	public static void main(String[] args) {
 		DrivingInterface driving = new DrivingInterface();
 		DrivingController controller = new DrivingController();
