63dfe8614bd0b63c091998914deb025b986cf84b
JohnyLee88 <superfreddie@naver.com>
2016-10-23 19:23:26 +0900

    트랙정보 데이터 객체

---
+++ DrivingInterface/src/TrackData.java
@@ -0,0 +1,28 @@
+
+public class TrackData {
+	
+	double track_length;
+	double track_width;
+	
+	public TrackData(double length, double width) {
+		
+		this.track_length = length;
+		this.track_width = width;
+		
+	}
+	
+	public double getTrack_length() {
+		return track_length;
+	}
+	public void setTrack_length(double track_length) {
+		this.track_length = track_length;
+	}
+	public double getTrack_width() {
+		return track_width;
+	}
+	public void setTrack_width(double track_width) {
+		this.track_width = track_width;
+	}
+	
+	
+}
